019280dfbe497328b3df6c21d9907c8821d8994b
Lakshay Bhatia <b.lakshay.aspect@gmail.com>
2026-08-18 20:39:10 +0530

    minor changes

--- chatapp-be/src/db/db.ts
+++ chatapp-be/src/db/db.ts
@@ -1,8 +1,9 @@
 import mongoose from "mongoose"
-
+import "dotenv/config";
+const URI = String(process.env.MONGODB_URI) || "";
 export default async function connectDB(){
     try{
-        await mongoose.connect("mongodb://127.0.0.1:27017/chatapp")
+        await mongoose.connect(URI)
         console.log("connected")
     }catch(e:any){
         console.log(e.message)
