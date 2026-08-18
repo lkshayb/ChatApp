569cdfba55ce58cff7a0a81ba497bb9e0967f417
Lakshay Bhatia <b.lakshay.aspect@gmail.com>
2026-08-18 20:40:25 +0530

    migrated frontend url from localhost to deployment

--- chatapp-fe/src/Pages/mvp.tsx
+++ chatapp-fe/src/Pages/mvp.tsx
@@ -46,8 +46,8 @@ export default function MainApp() {
   const connectWebSocket = () => {
     if (wsRef.current) return;
     setIsConnecting(true);
-    //const ws = new WebSocket("https://chatapp-ecai.onrender.com");
-    const ws = new WebSocket("ws://localhost:8080");
+    const ws = new WebSocket("https://chatapp-ecai.onrender.com");
+    //const ws = new WebSocket("ws://localhost:8080");
     
     ws.onopen = () => {
       console.log("WebSocket connection established");
