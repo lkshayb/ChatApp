088987ba9ccdea7f63cb84ef4b13a77637e2c2e1
Lakshay Bhatia <b.lakshay.aspect@gmail.com>
2026-08-18 20:54:40 +0530

    minor dpl changes

--- chatapp-be/src/db/db.ts
+++ chatapp-be/src/db/db.ts
@@ -1,9 +1,9 @@
 import mongoose from "mongoose"
-import "dotenv/config";
-const URI = String(process.env.MONGODB_URI) || "";
+import "dotenv/config"; 
+const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.vnt6tgn.mongodb.net/mydb`;
 export default async function connectDB(){
     try{
-        await mongoose.connect(URI)
+        await mongoose.connect(uri)
         console.log("connected")
     }catch(e:any){
         console.log(e.message)
